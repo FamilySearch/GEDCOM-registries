33263c2246de75b11cc02bee672eb9c7acb65a08
Dave Thaler <dthaler1968@gmail.com>
2025-08-15 18:38:52 -0700

    Update validator python script for v5.5.1
    Signed-off-by: Dave Thaler <dthaler1968@gmail.com>

--- registry_tools/validator.py
+++ registry_tools/validator.py
@@ -7,27 +7,62 @@ if '--help' in sys.argv or '-h' in sys.argv or '?' in sys.argv:
   print('If given no arguments, reads YAML from stdin')
   quit()
 
-count = 0
-ok = 0
-if len(sys.argv) > 1:
-  for arg in sys.argv[1:]:
-    count += 1
+base_dir = os.path.join(os.path.dirname(__file__), 'GEDCOM.io')
+try:
+  schema_v7 = yaml.safe_load(open(os.path.join(base_dir, 'yaml-schema.yaml')))
+  schema_v551 = yaml.safe_load(open(os.path.join(base_dir, 'yaml-schema-v5.5.1.yaml')))
+except Exception as ex:
+  print("Fatal Error loading schema(s):", ex, file=sys.stderr)
+  exit(1)
+
+def check(data, name, schemas):
+  success = True
+  for version, schema in schemas.items():
     try:
-      data = yaml.safe_load(open(arg))
+      validate(data, schema)
     except BaseException as ex:
       print("="*30, file=sys.stderr)
-      print("Failed to load", arg, file=sys.stderr)
+      print(f"Validation error with {name} against {version}", file=sys.stderr)
       print(ex, file=sys.stderr)
-      continue
-    if check(data, arg): ok += 1
+      success = False
+  return success
+
+def detect_versions(text):
+  versions = {}
+  if '/v7/' in text:
+    versions['v7'] = schema_v7
+  if '/v5.5.1/' in text:
+    versions['v5.5.1'] = schema_v551
+  if not versions:
+    versions['default (v7)'] = schema_v7
+  return versions
+
+count = 0
+ok = 0
+
+def process_yaml(source, name):
+  global ok, count
+  count += 1
+  try:
+    raw_text = source.read()
+    data = yaml.safe_load(raw_text)
+    schemas = detect_versions(raw_text)
+    if check(data, name, schemas): ok += 1
+  except BaseException as ex:
+    print("="*30, file=sys.stderr)
+    print("Failed to load", name, file=sys.stderr)
+    print(ex, file=sys.stderr)
+
+if len(sys.argv) > 1:
+  for arg in sys.argv[1:]:
+    with open(arg, 'r') as f:
+      process_yaml(f, arg)
 else:
-  for data in yaml.safe_load_all(sys.stdin):
-    count += 1
-    if check(data, 'YAML sent to stdin'): ok += 1
+  process_yaml(sys.stdin, 'YAML sent to stdin')
 
 if ok != count:
   print("="*30+'\n')
-print("YAML files checked:",count)
-print("YAML files passed:",ok)
+print("YAML files checked:", count)
+print("YAML files passed:", ok)
 if ok != count:
   sys.exit(1)
