5deb5053f9d310b8d7008f1c4ec2bfee5bdb6c60
Dave Thaler <dthaler1968@gmail.com>
2025-08-19 12:52:36 -0700

    Fix merge conflicts
    Signed-off-by: Dave Thaler <dthaler1968@gmail.com>

--- registry_tools/validator.py
+++ registry_tools/validator.py
@@ -1,4 +1,4 @@
-from validatorlib import check
+from validatorlib import check, detect_versions
 import yaml
 import sys
 
@@ -6,37 +6,6 @@ if '--help' in sys.argv or '-h' in sys.argv or '?' in sys.argv:
   print("USAGE:", sys.argv[0], 'file_to_validate.yaml', '[another_file.yaml ...]')
   print('If given no arguments, reads YAML from stdin')
   quit()
-
-import os.path
-base_dir = os.path.join(os.path.dirname(__file__), 'GEDCOM.io')
-try:
-  schema_v7 = yaml.safe_load(open(os.path.join(base_dir, 'yaml-schema.yaml')))
-  schema_v551 = yaml.safe_load(open(os.path.join(base_dir, 'yaml-schema-v5.5.1.yaml')))
-except Exception as ex:
-  print("Fatal Error loading schema(s):", ex, file=sys.stderr)
-  exit(1)
-
-def check(data, name, schemas):
-  success = True
-  for version, schema in schemas.items():
-    try:
-      validate(data, schema)
-    except BaseException as ex:
-      print("="*30, file=sys.stderr)
-      print(f"Validation error with {name} against {version}", file=sys.stderr)
-      print(ex, file=sys.stderr)
-      success = False
-  return success
-
-def detect_versions(text):
-  versions = {}
-  if '/v7/' in text:
-    versions['v7'] = schema_v7
-  if '/v5.5.1/' in text:
-    versions['v5.5.1'] = schema_v551
-  if not versions:
-    versions['default (v7)'] = schema_v7
-  return versions
 
 count = 0
 ok = 0
