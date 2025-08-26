45a241e1b7c2d2b4bfd1e2ac859df50973fbb6fe
Dave Thaler <dthaler1968@gmail.com>
2025-08-26 09:35:30 -0700

    Add global-validator to the validate-yaml github workflow (#198)
    * Make global-validation part of the github workflow
    
    Also fix bad merge
    
    Signed-off-by: Dave Thaler <dthaler@armidalesoftware.com>
    
    * Remove g551validation.json
    
    Fixes #199
    
    Signed-off-by: Dave Thaler <dthaler@armidalesoftware.com>
    
    ---------
    
    Signed-off-by: Dave Thaler <dthaler@armidalesoftware.com>
    Co-authored-by: Dave Thaler <dthaler@armidalesoftware.com>

--- registry_tools/validatorlib.py
+++ registry_tools/validatorlib.py
@@ -7,7 +7,7 @@ base_dir = os.path.join(os.path.dirname(__file__), 'GEDCOM.io')
 try:
   schema_v7 = yaml.safe_load(open(os.path.join(base_dir, 'yaml-schema.yaml')))
   schema_v551 = yaml.safe_load(open(os.path.join(base_dir, 'yaml-schema-v5.5.1.yaml')))
-except:
+except Exception as ex:
   print("Fatal error loading schema(s):", ex, file=sys.stderr)
   exit(1)
 
